3600bcb5d2bf236c65fd19440de4bd4433bb1f95
logicp <logicp@logicp.ca>
2022-10-12 01:11:00 -0400

    adding test

--- test/kettr.test.cpp
+++ test/kettr.test.cpp
@@ -1,10 +1,6 @@
 #include "kettr.test.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
+TEST(KettrTest, Login)
 {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  EXPECT_TRUE((kettr{"email", "password"}.login()));
 }
